471249f196718220f3def963bb9842331c749029
Kamil Milewski <kmilewski@student.agh.edu.pl>
2023-04-21 23:17:01 +0200

    zad 2 poprawione

--- lab7/zad2.c
+++ lab7/zad2.c
@@ -3,65 +3,90 @@
 #include <string.h>
 
 
-char** podziel(char* lancuch, int n)
+char** podziel(char* lancuch, int *n)
 {
-    char *buff = strtok(lancuch, " ,;:.");
-    int i = 1;
-    char *slowa[n];
+    char**tablica = malloc(sizeof(char*));
+    char podzielniki[]=" .;:,";
+    int i = 0;//ilosc wyrazow
+    char* buff = strtok(lancuch, podzielniki);
     
-    while(buff!=NULL){
-      
-        int nowy = 1; //sprawdza czy dany wyraz pojawil sie wczesniej
-        for (int j = 0; j< i;j++)
-        {
-            if(strcmp(buff, slowa[j])==0)
-                nowy=0;
-        }
-    if(nowy)
+    while(buff!=NULL)
     {
-        slowa[i-1] = buff;
-        i++;
+        int nowy = 1;
+        for(int j = 0; j<i;j++)
+        {
+            if(strcmp(tablica[j], buff)==0)
+                nowy = 0;
+        }
+        if (nowy)
+        {
+            tablica[i]=buff;
+            i++;
+            tablica = realloc(tablica, (i+1)*sizeof(char*));
+        }
+        buff=strtok(NULL,podzielniki);
     }
-    buff = strtok(NULL, " ,:;.");
-    }
-    return slowa;
+    tablica = realloc(tablica, i*sizeof(char*));//usuniecie nadmiarowego rozmiaru tablicy po skonczeniu sie wyrazow
+    *n = i;//zapamietanie ilosci elementow w tablicy slow
+    return tablica;
 }
 
 void sort(char** tablica, int n)
 {
-    int min;
-    for (int i = 0; i < n-1; i++)
+    for(int i = 0; i<(n-1); i++)
     {
-        min = i;
-        for (int j = 1+i;j<n;j++)
+        int min = i;
+        for (int j = i+1; j<n; j++)
         {
-            if(strcmp(tablica[i], tablica[min])<0)
-            min=j;
+            if(strcmp(tablica[j], tablica[min])<0)
+                min = j;
         }
-        char*buff = tablica[min];
-        tablica[min] = tablica [i];
-        tablica[i] = buff;
+        char*buff=tablica[min];
+        tablica[min]=tablica[i];
+        tablica[i]=buff;
     }
 }
-char* sklej(char** tablica, int n)
+
+char* sklej(char** lancuch, int n)
 {
-    char*tab = malloc(strlen((tablica[0])+1)*sizeof(char));
-    tab = strcpy(tab, tablica[0]);
-    for (int i =1; i<n;i++)
+    int rozmiar = 0;
+    for(int i = 0; i<n;i++)
     {
-        tab = realloc(tab, (strlen(tab)+strlen(tablica[i])+1)*sizeof(char));
-        tab = strcat(tab, " ");
-        tab = strcat(tab, tablica[i]);
+        rozmiar +=(strlen(lancuch[i])+1);
     }
+    printf("Rozmiar:%d\n",rozmiar);
+    char* str = malloc(rozmiar*sizeof(char));
+    str=strcpy(str, lancuch[0]);
+    for (int i = 1; i<n;i++)
+    {
+        str = strcat(str, " ");
+        str=strcat(str, lancuch[i]);
+    }
+    str[rozmiar-1] = '\0';
+    return str;
 }
+
 int main (int argc, char*argv[])
 {
-
 char* arg = argv[1];
-char**wynik = podziel(arg, strlen(arg));
-sort(wynik, strlen(arg));
-char* string = sklej (wynik, strlen(arg));
-free (string);
+int licznik = 0;
+char**wynik = podziel(arg, &licznik);
+for(int i = 0; i<licznik; i++)
+{
+    printf("%s\n", wynik[i]);
+}
+sort(wynik, licznik);
+printf("Po posortowaniu:\n");
+for(int i = 0; i<licznik; i++)
+{
+    printf("%s\n", wynik[i]); 
+}
+char* sklejony = sklej(wynik, licznik);
+printf("Wyraz po sklejeniu: %s\n", sklejony);
 
+
+
+free (wynik);
+free(sklejony);
     return 0;
 }
